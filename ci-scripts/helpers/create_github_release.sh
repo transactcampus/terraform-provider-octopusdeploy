91d1a504f6d431939856dcfbbff304c86ff4fcd2
jonyfq <dmitry.smirnov@usabilitydynamics.com>
2020-11-17 16:13:59 +0200

    implemented build and release process ref DE-117

--- ci-scripts/helpers/create_github_release.sh
+++ ci-scripts/helpers/create_github_release.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 
-if [ -z "${RELEASE_VERSION}" ]; then
-    echo "The environment variable RELEASE_VERSION needs to be set. Exiting script."
-    exit 1
-fi
+# if [ -z "${RELEASE_VERSION}" ]; then
+#     echo "The environment variable RELEASE_VERSION needs to be set. Exiting script."
+#     exit 1
+# fi
 
 go get -u github.com/tcnksm/ghr
 
-ghr -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${RELEASE_VERSION} ./artifacts/
+REPOSITORY_NAME=$(basename `git rev-parse --show-toplevel`)
+
+ghr -u ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} -delete ${RELEASE_VERSION} ./artifacts/
--- ci-scripts/helpers/get_release_version.sh
+++ ci-scripts/helpers/get_release_version.sh
@@ -4,12 +4,30 @@
 
 set -eo pipefail
 
-if [ -z "${CIRCLE_BUILD_NUM}" ]; then
-    echo "The environment variable CIRCLE_BUILD_NUM is not set. Setting as 999."
-    CIRCLE_BUILD_NUM="999"
-fi
+# if [ -z "${CIRCLE_BUILD_NUM}" ]; then
+#     echo "The environment variable CIRCLE_BUILD_NUM is not set. Setting as 999."
+#     CIRCLE_BUILD_NUM="999"
+# fi
 
-RELEASE_VERSION="0.0.2-alpha.${CIRCLE_BUILD_NUM}"
+# RELEASE_VERSION="0.0.2-alpha.${CIRCLE_BUILD_NUM}"
+# echo "Release version is ${RELEASE_VERSION}"
+
+# export RELEASE_VERSION=$RELEASE_VERSION
+
+# Get latest tag ref
+REF=$(git rev-list --tags --max-count=1)
+
+# Get latest tag 
+LATEST_TAG=$(git describe --tags ${REF}) 
+
+# Remove suffix
+LATEST_TAG=${LATEST_TAG/v/''}
+
+# Determine next version
+RELEASE_VERSION=`echo $LATEST_TAG | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+
+# Add suffix
+RELEASE_VERSION="v${RELEASE_VERSION}"
+
 echo "Release version is ${RELEASE_VERSION}"
-
 export RELEASE_VERSION=$RELEASE_VERSION
