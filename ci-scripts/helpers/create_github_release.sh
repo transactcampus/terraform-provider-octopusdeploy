9956f10b476e710746ba187dfb55914d223c3060
jonyfq <dmitry.smirnov@usabilitydynamics.com>
2020-11-17 17:50:27 +0200

    added get_release_version to create_github_release ref DE-117

--- ci-scripts/helpers/create_github_release.sh
+++ ci-scripts/helpers/create_github_release.sh
@@ -10,5 +10,7 @@ go get -u github.com/tcnksm/ghr
 
 REPOSITORY_NAME=$(basename `git rev-parse --show-toplevel`)
 
+ci-scripts/get_release_version.sh
+
 # ghr -t ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} -delete ${RELEASE_VERSION} ./artifacts/
 ghr -t ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} ${RELEASE_VERSION} ./artifacts/
