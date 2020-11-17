40b1704cca288f71444d4fa10e00e2c58475a2a6
jonyfq <dmitry.smirnov@usabilitydynamics.com>
2020-11-17 17:23:33 +0200

    fixed tag name for github release ref DE-117

--- ci-scripts/helpers/create_github_release.sh
+++ ci-scripts/helpers/create_github_release.sh
@@ -11,4 +11,4 @@ go get -u github.com/tcnksm/ghr
 REPOSITORY_NAME=$(basename `git rev-parse --show-toplevel`)
 
 # ghr -t ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} -delete ${RELEASE_VERSION} ./artifacts/
-ghr -t ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} ./artifacts/
+ghr ${RELEASE_VERSION} -t ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} ./artifacts/
