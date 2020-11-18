8e34b15ebd5eeab8a9ecf36f4bd9009c1f155f9c
jonyfq <dmitry.smirnov@usabilitydynamics.com>
2020-11-18 09:47:21 +0200

    fixes

--- ci-scripts/helpers/create_github_release.sh
+++ ci-scripts/helpers/create_github_release.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 
+# Get params
+LATEST_RELEASE=$1
+API_TOKEN=$2
+BRANCH=$3
+
 # Get and export release version
-. ci-scripts/helpers/get_release_version.sh $1
+. ci-scripts/helpers/get_release_version.sh $LATEST_RELEASE
 
 # Download the package
 go get -u github.com/tcnksm/ghr
