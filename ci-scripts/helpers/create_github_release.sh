8f605c68a9a186a4e209671e71040678a16fa5a9
jonyfq <dmitry.smirnov@usabilitydynamics.com>
2020-11-18 09:13:55 +0200

    echo release details ref DE-117

--- ci-scripts/helpers/create_github_release.sh
+++ ci-scripts/helpers/create_github_release.sh
@@ -10,5 +10,7 @@ go get -u github.com/tcnksm/ghr
 # Get repository name
 REPOSITORY_NAME=$(basename `git rev-parse --show-toplevel`)
 
+echo "Creating new release ${REPOSITORY_NAME}/${BRANCH}/${RELEASE_VERSION}"
+
 # Create release
 ghr -t ${API_TOKEN} -r ${REPOSITORY_NAME} -c ${BRANCH} ${RELEASE_VERSION} ./artifacts/
